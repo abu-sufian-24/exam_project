b2116e9896dd705d5f5e5e02a8d8d6e4dee61dcb
abu-sufian-24 <abusufian300400@gmail.com>
2024-11-18 17:12:13 +0600

    done

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -9,8 +9,8 @@ const Navbar = () => {
   const { setShowAddToCartPopup, cartItem } = useContext(BookContext)
   return (
     <nav className=" bg-gray-800 px-4 py-4 fixed w-full top-0 z-50">
-      <div className="container mx-auto flex justify-between items-center px-4 md:px-8 lg:px-24 ml-20 sm:ml-0">
-        <a className=" hidden sm:block text-xl md:text-2xl font-bold text-divineGreen">DivineBook</a>
+      <div className="container mx-auto flex justify-between items-center px-4 md:px-8 lg:px-24 ml-20 md:ml-0">
+        <a className=" hidden md:block text-xl md:text-2xl font-bold text-divineGreen">DivineBook</a>
         <div className="flex items-center space-x-3 md:space-x-5">
           <div className="w-8 h-8 md:w-10 md:h-10 bg-customGreen rounded flex justify-center items-center border-[#91EFD0]">
             <IoNotifications className="text-divineGreen text-xl md:text-2xl" />
--- src/component/sections/MeinSection.jsx
+++ src/component/sections/MeinSection.jsx
@@ -16,7 +16,7 @@ function MeinSection() {
       <section className="bg-gray-900 dark:bg-white mt-16">
         <div className="max-w-[1420px] mx-auto text-white">
           <div className="bg-gray-800 dark:bg-white p-4 sm:p-8 rounded-lg">
-            <div className="grid grid-cols-1 md:grid-cols-12 gap-4 sm:gap-6">
+            <div className="grid grid-cols-1 lg:grid-cols-12 gap-4 sm:gap-6">
 
               {/* Left Sidebar - 2 columns on medium screens */}
               <div className="col-span-12 md:col-span-2">
